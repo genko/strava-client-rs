6a94ad0bede2f38c6736524017107b17545b15d1
qgriffith <qgriffith@gmail.com>
2024-02-11 12:26:14 -0500

    Added documentation and examples for the util modules

--- src/util/auth_config.rs
+++ src/util/auth_config.rs
@@ -1,20 +1,36 @@
+/// # auth_config.rs
+/// Checks to see if a config file exists and pulls the access_code and refresh_token
+/// Can create a config file that stores the access_code and refresh_token if not found
+/// # Example
+/// ```no_run
+///  // Check if the config file exists and get the access token or get a new one
+///  if Path::new(&config_file).exists() {
+///     config.refresh_token = Some(auth_config::config_file::load_config().refresh_token);
+///     let refresh_access_token = auth::get_refresh_token(config);
+///     Ok(refresh_access_token.unwrap().to_string())
+///  } else {
+///      let access_token = auth::get_authorization(config);
+///     Ok(access_token.unwrap().to_string())
+///  }
+/// ```
 use log::{trace, warn};
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs::File;
 
+/// ## config_file module to create a config file or retrieve the access_token, refresh_token
 pub mod config_file {
 
     use super::*;
 
-    // ConfigFile struct to hold the access token and refresh token
+    /// ConfigFile struct to hold the access token and refresh token
     #[derive(Debug, Serialize, Deserialize)]
     pub struct ConfigFile {
         pub access_token: String,
         pub refresh_token: String,
     }
 
-    // Private function to get the config file from the environment variable STRAVA_CONFIG_FILE or set it to config.json
+    /// Private function to get the config file from the environment variable STRAVA_CONFIG_FILE or set it to config.json
     fn get_config_file() -> String {
         let config_file =
             env::var("STRAVA_CONFIG_FILE").unwrap_or_else(|_| "config.json".to_string());
@@ -22,7 +38,7 @@ pub mod config_file {
         config_file
     }
 
-    // Write the access token and refresh token to the config file
+    /// Write the access token and refresh token to the config file
     pub fn write_config(access_token: &String, refresh_token: &String) {
         let config = ConfigFile {
             access_token: access_token.to_string(),
@@ -41,8 +57,8 @@ pub mod config_file {
         });
     }
 
-    // Load the config file from JSON and return a Config struct.
-    //Returns a Config struct or panics if it can't open or read the file.
+    /// Load the config file from JSON and return a Config struct.
+    /// Returns a Config struct or panics if it can't open or read the file.
     pub fn load_config() -> ConfigFile {
         let get_file = get_config_file();
 
